dc2f8ee58f4088f7558643259fdb989235173636
tk1rk <taylor.kirk@pm.me>
2021-10-31 11:47:19 -0700

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,9 @@
 -- INIT.LUA --
+require "paq" {
+    "savq/paq-nvim";                  -- Let Paq manage itself
+
+    "neovim/nvim-lspconfig";          -- Mind the semi-colons
+    "hrsh7th/nvim-compe";
+
+    {"lervag/vimtex", opt=true};      -- Use braces when passing options
+}
