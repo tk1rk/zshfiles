584213dfa922e72d30af856bb1f3ee4f59e5476a
tk1rk <taylor.kirk@pm.me>
2021-11-07 20:52:06 -0800

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,17 +2,25 @@ require('settings')    -- lua/settings.lua
 require('maps')        -- lua/maps.lua
 require('statusline')  -- lua/statusline.lua
 
-vim.cmd 'colorscheme material-dracula'
 
+-- THEME --
+local cmd = vim.cmd
+vim.cmd(':colorscheme material_dracula')
+
+
+----------------------
+-- Install paq-nvim --
 local fn = vim.fn
-
+----------------------
 local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
-
+----------------------
 if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
 end
 
--- INIT.LUA --
+
+--------------
+-- PAQ-NVIM --
 require "paq-nvim" {
     "savq/paq-nvim";                  -- Let Paq manage itself
     "neovim/nvim-lspconfig";          -- Mind the semi-colons
@@ -23,3 +31,5 @@ require "paq-nvim" {
     'ray-x/material_plus.nvim';
     "lervag/vimtex", opt=true};      -- Use braces when passing options
 }
+
+
