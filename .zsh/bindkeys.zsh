c8a9cd398627d0a5dfb93036660165bd2858fbe5
tk1rk <taylor.kirk@pm.me>
2021-12-19 17:20:31 -0800

    Update bindkeys.zsh

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -13,3 +13,23 @@ bindkey "^D"      delete-char                          # ctrl-d
 bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
 bindkey -e   # Default to standard emacs bindings, regardless of editor stringeb
+
+# fuzzy find: start to type
+bindkey "$terminfo[kcuu1]" up-line-or-beginning-search
+bindkey "$terminfo[kcud1]" down-line-or-beginning-search
+bindkey "$terminfo[cuu1]" up-line-or-beginning-search
+bindkey "$terminfo[cud1]" down-line-or-beginning-search
+
+# backward and forward word with option+left/right
+bindkey '^[^[[D' backward-word
+bindkey '^[b' backward-word
+bindkey '^[^[[C' forward-word
+bindkey '^[f' forward-word
+
+# delete char with backspaces and delete
+bindkey '^[[3~' delete-char
+bindkey '^?' backward-delete-char
+
+# delete word with ctrl+backspace
+bindkey '^[[3;5~' backward-delete-word
+# bindkey '^[[3~' backward-delete-word
