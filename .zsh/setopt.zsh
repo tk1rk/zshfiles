ee4bb23bff1a297d0cb0a89f1d02a318535cae25
tk1rk <taylor.kirk@pm.me>
2021-12-22 10:32:07 -0800

    Update setopt.zsh

--- .zsh/setopt.zsh
+++ .zsh/setopt.zsh
@@ -1,8 +1,26 @@
 #!/bin/zsh
 
-# Set some history options
+
+# Changing Directories
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushd_silent
+
+# Completion
+setopt auto_param_slash
+setopt complete_in_word
+setopt glob_complete
+setopt list_beep
+setopt list_packed
+setopt list_rows_first
+setopt no_beep
+
+# History
 setopt append_history
-setopt extended_history
+setopt share_history
+unsetopt bang_hist
+unsetopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_all_dups
 setopt hist_ignore_dups
@@ -12,6 +30,9 @@ setopt hist_save_no_dups
 setopt hist_verify
 setopt INC_APPEND_HISTORY
 unsetopt HIST_BEEP
+
+# Job Control
+setopt notify
 
 # Share your history across all your terminal windows
 setopt share_history
