31a9e3527509b1ce31a5fc85abebf984312150d9
tk1rk <taylor.kirk@pm.me>
2021-12-22 10:44:06 -0800

    Update bindkeys.zsh

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -33,3 +33,71 @@ bindkey '^?' backward-delete-char
 # delete word with ctrl+backspace
 bindkey '^[[3;5~' backward-delete-word
 # bindkey '^[[3~' backward-delete-word
+
+
+# Load menu-style completion.
+zmodload -i zsh/complist
+bindkey -M menuselect '^M' accept
+
+# Show dots while waiting to complete. Useful for systems with slow net access,
+# like those places where they use giant, slow NFS solutions. (Hint.)
+expand-or-complete-with-dots() {
+echo -n "\e[31m......\e[0m"
+zle expand-or-complete
+zle redisplay
+}
+zle -N expand-or-complete-with-dots
+bindkey "^I" expand-or-complete-with-dots
+
+# This inserts a tab after completing a redirect. You want this.
+# (Source: http://www.zsh.org/mla/users/2006/msg00690.html)
+self-insert-redir() {
+integer l=$#LBUFFER
+zle self-insert
+(( $l >= $#LBUFFER )) && LBUFFER[-1]=" $LBUFFER[-1]"
+}
+zle -N self-insert-redir
+for op in \| \< \> \& ; do
+  bindkey "$op" self-insert-redir
+done
+
+# Automatically quote URLs when pasted
+autoload -U url-quote-magic
+zle -N self-insert url-quote-magic
+
+# ZSH KEYBINDINGS {{{1
+
+# First, primarily use emacs key bindings
+bindkey -e
+
+# One keystroke to cd ..
+bindkey -s '\eu' '\eq^Ucd ..; ls^M'
+
+# Smart less-adder
+bindkey -s "\el" "^E 2>&1|less^M"
+
+# This lets me use ^Z to toggle between open text editors.
+bindkey -s '^Z' '^Ufg^M'
+
+# More custom bindings
+bindkey "^O" copy-prev-shell-word
+bindkey "^Q" push-line
+bindkey "^T" history-incremental-search-forward
+bindkey "ESC-." insert-last-word
+
+# Edit the current command line with Meta-e
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '\ee' edit-command-line
+
+# Let ^W delete to slashes - zsh-users list, 4 Nov 2005
+# (I can't live without this)
+backward-delete-to-slash() {
+  local WORDCHARS=${WORDCHARS//\//}
+  zle .backward-delete-word
+}
+zle -N backward-delete-to-slash
+bindkey "^W" backward-delete-to-slash
+
+# AUTO_PUSHD is set so we can always use popd
+bindkey -s '\ep' '^Upopd >/dev/null; dirs -v^M'
