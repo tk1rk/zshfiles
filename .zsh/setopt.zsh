37c37dfcfa9ed86f91979419b73261412e69400e
tk1rk <taylor.kirk@pm.me>
2021-12-23 12:26:18 -0800

    Update setopt.zsh

--- .zsh/setopt.zsh
+++ .zsh/setopt.zsh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
+# 16.2.1 Changing Directories {{{2
+
+setopt AUTO_CD            # Change to a directory just by typing its name
+setopt AUTO_PUSHD         # Make cd push each old directory onto the stack
+setopt CDABLE_VARS        # Like AUTO_CD, but for named directories
+setopt PUSHD_IGNORE_DUPS  # Don't push duplicates onto the stack
+
+
 # Set some history options
 setopt append_history
 setopt extended_history
@@ -27,9 +35,6 @@ export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
 # Set some options about directories
 setopt pushd_ignore_dups
 #setopt pushd_silent
-setopt AUTO_CD  # If a command is issued that can’t be executed as a normal command,
-                # and the command is the name of a directory, perform the cd command
-                # to that directory.
 
 # Add some completions settings
 setopt ALWAYS_TO_END     # Move cursor to the end of a completed word.
@@ -38,6 +43,8 @@ setopt AUTO_MENU         # Show completion menu on a successive tab press.
 setopt AUTO_PARAM_SLASH  # If completed parameter is a directory, add a trailing slash.
 setopt COMPLETE_IN_WORD  # Complete from both ends of a word.
 setopt CORRECT
+setopt NO_LIST_BEEP       # Don't beep on an ambiguous completion
+setopt LIST_PACKED        # Use columns of varying widths
 unsetopt MENU_COMPLETE   # Do not autoselect the first completion entry.
 
 # Miscellaneous settings
